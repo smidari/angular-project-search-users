048384866ef6b8cbfa686506486126ea1ab7bc52
Dasha <smidariaa@gmail.com>
2020-11-30 17:33:10 +0300

    Modified the home component:

--- src/app/modules/share/components/all-users/all-users.component.ts
+++ src/app/modules/share/components/all-users/all-users.component.ts
@@ -1,5 +1,11 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 import { User } from '../../../../user';
+
 
 @Component({
   selector: 'app-all-users',
@@ -8,11 +14,21 @@ import { User } from '../../../../user';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AllUsersComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'avatar',
+    'first_name',
+    'last_name',
+    'email',
+
+  ];
+
   @Input()
   users: User[];
   constructor() {}
 
   ngOnInit(): void {
+    console.log(this.users);
   }
 
   trackById(index, item): number {
